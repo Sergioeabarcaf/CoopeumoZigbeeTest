ef7131541ab67f8ea9bbfe97e8b5ff7c886765dd
Sergio Abarca Flores <sergioeabarcaf@MacBook-Air-de-Sergio.local>
2019-09-26 21:05:37 -0300

    SERVER - 5 datos, mensajes y sincronizar grive

--- serverPivot/main.py
+++ serverPivot/main.py
@@ -8,6 +8,7 @@ import timeCustom
 import log
 import sys
 import csvFile
+import os
 
 id = ["00:13:A2:00:41:54:B4:F0","00:13:A2:00:41:92:E0:1F","00:13:A2:00:41:9B:43:E4","00:13:A2:00:41:54:B4:EE","00:13:A2:00:41:5B:67:F2"]
 
@@ -15,18 +16,24 @@ try:
     while (True):
         if (conection.valid()):
             for disp in id:
-                cant = 10
+                cant = 5
                 # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
                 data = json.loads(drm.getDataDevice(disp,cant))
+                # Revertir orden de la data
                 dataReverse = data['list'][::-1]
                 for x in dataReverse:
+                    # Decodificar mensaje
                     dataValue = base64.b64decode(x['value'])
+                    #Extraer tiempo del mensaje
                     dataTime = x['timestamp']
                     # Limpiar la data y obtener el dispositivo con sus valores
                     data = drm.obtenerData(dataValue)
+                    # si el dato es nuevo, se envia a firebase y se guarda en CSV
                     if goToFirebase.checkData(dataTime, disp):
                         goToFirebase.send(dataTime,disp,data)
                         csvFile.writeData(disp, dataTime, (dataTime.split("T")[0] + '.csv'), data)
+            # actualizar el contenido en carpeta Drive
+            os.system("grive -u -s datos/")
         else:
             log.recivedLog(timeCustom.getCurrenDateAndTimeSTR())
         time.sleep(50)
