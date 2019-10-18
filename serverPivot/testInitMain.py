6691caee5f820784f3384adeb9618db18f4ece50
Sergio Abarca Flores <sergioeabarcaf@MacBook-Air-de-Sergio.local>
2019-10-18 11:18:20 -0300

    SERVER - cambio de fechas y creacion de testInitMain

---
+++ serverPivot/testInitMain.py
@@ -0,0 +1,48 @@
+import goToFirebase
+import drm
+import json
+import base64
+import time
+import conection
+import timeCustom
+import log
+import sys
+import csvFile
+import os
+
+id = ["00:13:A2:00:41:54:B4:F0","00:13:A2:00:41:92:E0:1F","00:13:A2:00:41:9B:43:E4","00:13:A2:00:41:54:B4:EE","00:13:A2:00:41:5B:67:F2"]
+dates = ["2019-10-16T00:00:00.0","2019-10-16T12:00:00.0","2019-10-17T00:00:00.0","2019-10-17T12:00:00.0",
+         "2019-10-18T00:00:00.0","2019-10-18T12:00:00.0","2019-10-19T00:00:00.0","2019-10-19T12:00:00.0",
+         "2019-10-20T00:00:00.0","2019-10-20T12:00:00.0","2019-10-21T00:00:00.0","2019-10-21T12:00:00.0",
+         "2019-10-22T00:00:00.0","2019-10-22T12:00:00.0","2019-10-23T00:00:00.0","2019-10-23T12:00:00.0",
+         "2019-10-24T00:00:00.0","2019-10-16T24:00:00.0","2019-10-25T00:00:00.0","2019-10-25T12:00:00.0",]
+
+cant = 750
+
+if (conection.valid()):
+    while (cant > 0):
+        for disp in id:
+            for date in dates:
+                # Obtener el ultimo dato desde Digi Remote y convertirla en un JSON
+                data = json.loads(drm.getDataInit(disp,cant,date))
+                # Revertir orden de la data
+                dataReverse = data['list'][::-1]
+                for x in dataReverse:
+                    # Decodificar mensaje
+                    dataValue = base64.b64decode(x['value'])
+                    #Extraer tiempo del mensaje
+                    dataTime = x['timestamp']
+                    # Limpiar la data y obtener el dispositivo con sus valores
+                    data = drm.obtenerData(dataValue)
+                    # Se guarda el dato en CSV y si el dato es nuevo, se envia a firebase
+                    if goToFirebase.checkData(dataTime, disp):
+                        goToFirebase.send(dataTime,disp,data)
+                        csvFile.writeData(disp, dataTime, (dataTime.split("T")[0] + '.csv'), data)
+                log.recivedAll(timeCustom.getCurrenDateAndTimeSTR(), "revisado " + str(disp) + " en fecha " + str(date) + " con una cantidad de " + str(cant))
+            # actualizar el contenido en carpeta Drive
+            os.system("grive -u -s datos/")
+            log.recivedAll(timeCustom.getCurrenDateAndTimeSTR(), "Se actualizo GRIVE del dispositivo " + str(disp))
+            if (cant > 50):
+                cant -= 50
+else:
+    log.recivedLog(timeCustom.getCurrenDateAndTimeSTR())
